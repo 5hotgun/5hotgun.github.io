12a3af0f4048b3dd1f2378518c1a9f4955c7383a
5hotgun <47235013+5hotgun@users.noreply.github.com>
2019-03-15 17:10:03 +0600

    Header

---
+++ victoria-catering/assets/js/callModal.js
@@ -0,0 +1,8 @@
+let callModalClosed = true;
+const showCallModal = () => {
+    let callButtonPosition = callModalClosed ? -50 : 0;
+    let callModalPosition = callModalClosed ? 0 : -100;
+    $('.call-button-inner').css('transform', `translateX(${callButtonPosition}%)`);
+    $('.call-modal').css('right', `${callModalPosition}%`);
+    callModalClosed = !callModalClosed;
+}
