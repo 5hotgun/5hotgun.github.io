1308ec5a32504b9e669199cc1e2a9b42c5076035
5hotgun <6reengo@gmail.com>
2019-03-20 15:46:33 +0600

    Website v1

--- website_white/assets/js/callModal.js
+++
@@ -1,8 +0,0 @@
-let callModalClosed = true;
-const showCallModal = () => {
-    let callButtonPosition = callModalClosed ? -50 : 0;
-    let callModalPosition = callModalClosed ? 0 : -100;
-    $('.call-button-inner').css('transform', `translateX(${callButtonPosition}%)`);
-    $('.call-modal').css('right', `${callModalPosition}%`);
-    callModalClosed = !callModalClosed;
-};
