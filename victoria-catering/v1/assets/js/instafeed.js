0cad34ae9700360f11f86afc89e2fb295b5d2d47
5hotgun <6reengo@gmail.com>
2019-03-25 22:08:21 +0600

    Instagram update

--- victoria-catering/v1/assets/js/instafeed.js
+++ victoria-catering/v1/assets/js/instafeed.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
         resolution: 'standard_resolution',
         accessToken: '9134311101.1677ed0.9635943ef2ed498ea775853311c4efc3',
         sortBy: 'most-recent',
-        template: '<div class="insta-image"><a href="{{link}}" target="_blank"><img src="{{image}}" /></a></div>'
+        template: '<div class="insta-image"> <div class="insta-overlay"> <a href="{{link}}" target="_blank" class="insta-link"></a> <div class="insta-counts"> <div class="insta-count"><img src="assets/img/icons/like.svg" alt="Likes"><span>{{likes}}</span></div><div class="insta-count"><img src="assets/img/icons/comment.svg" alt="Comments"><span>{{comments}}</span></div></div></div><img src="{{image}}" alt="Instagram Photo"> </div>'
     });
     userFeed.run();
 });
