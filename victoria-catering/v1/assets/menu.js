94f137cd771de9fe2f920eb1d15f698a16ccfd86
5hotgun <6reengo@gmail.com>
2019-03-27 14:03:24 +0600

    Menu page added

---
+++ victoria-catering/v1/assets/menu.js
@@ -0,0 +1,65 @@
+const menu = [
+    {
+        menuList: 'Кофе-брейк',
+        menuGroups: [
+            {
+                groupTitle: 'Мини-сэндвичи',
+                groupItems: [
+                    {
+                        productName: 'Мини-сэндвич с семгой',
+                        productDesc: '',
+                        productWeight: '45 г.',
+                        productPrice: '110 руб.'
+                    },
+                    {
+                        productName: 'Мини-сэндвич с куриной грудкой и беконом',
+                        productDesc: '',
+                        productWeight: '45 г.',
+                        productPrice: '80 руб.'
+                    },
+                    {
+                        productName: 'Мини-сэндвич с ветчиной и сыром',
+                        productDesc: '',
+                        productWeight: '45 г.',
+                        productPrice: '80 руб.'
+                    },
+                    {
+                        productName: 'Мини-сэндвич с печеными овощами и алдыгейским сыром',
+                        productDesc: '',
+                        productWeight: '45 г.',
+                        productPrice: '80 руб.'
+                    }
+                ]
+            },
+            {
+                groupTitle: 'Мини-бейглы',
+                groupItems: [
+                    {
+                        productName: 'Мини-бейгл с семгой и сливочным сыром',
+                        productDesc: 'Слабосоленая семга, свежий огурец, салат айсберг',
+                        productWeight: '40 г.',
+                        productPrice: '110 руб.'
+                    },
+                    {
+                        productName: 'Мини-бейгл с с куриной грудкой',
+                        productDesc: '',
+                        productWeight: '40 г.',
+                        productPrice: '75 руб.'
+                    },
+                    {
+                        productName: 'Мини-бейгл с сыром моцарелла и свежими томатами',
+                        productDesc: '',
+                        productWeight: '40 г.',
+                        productPrice: '75 руб.'
+                    },
+                    {
+                        productName: 'Мини-бейгл с домашней бужениной и сливочным маслом',
+                        productDesc: '',
+                        productWeight: '40 г.',
+                        productPrice: '75 руб.'
+                    }
+                ]
+            }
+        ]
+    }
+];
