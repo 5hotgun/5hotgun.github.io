bf99f6b80db83f0036a1f42e83407454774afa63
5hotgun <6reengo@gmail.com>
2019-04-05 02:10:35 +0600

    Fix

--- victoria-catering/v1/assets/js/budgetSlider.js
+++ victoria-catering/v1/assets/js/budgetSlider.js
@@ -1,10 +1,8 @@
-$(document).ready(function() {
-    // Записать изначальное значение input range
-    let budgetValue = document.getElementById('budget').value;
-    document.getElementById('budgetValue').innerHTML = budgetValue;
-    // При изменении значения записывать новое
-    document.getElementById('budget').addEventListener('input', evt => {
-        let budgetChangedValue = document.getElementById('budget').value;
-        document.getElementById('budgetValue').innerHTML = budgetChangedValue;
-    });
+// Записать изначальное значение input range
+let budgetValue = document.getElementById('budget').value;
+document.getElementById('budgetValue').innerHTML = budgetValue;
+// При изменении значения записывать новое
+document.getElementById('budget').addEventListener('input', evt => {
+    let budgetChangedValue = document.getElementById('budget').value;
+    document.getElementById('budgetValue').innerHTML = budgetChangedValue;
 });
